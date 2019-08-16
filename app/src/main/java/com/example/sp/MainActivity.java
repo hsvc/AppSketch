b7df99d9ad6df98c1524f0f56d5b45bd6a0c94b7
jihyyeshin <tlsjh082@gmail.com>
2019-08-16 12:00:13 +0900

    Edit Layout

--- app/src/main/java/com/example/sp/MainActivity.java
+++ app/src/main/java/com/example/sp/MainActivity.java
@@ -5,16 +5,13 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 import okhttp3.MediaType;
---
+++ app/src/main/java/com/example/sp/ResultActivity.java
@@ -0,0 +1,12 @@
+package com.example.sp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
